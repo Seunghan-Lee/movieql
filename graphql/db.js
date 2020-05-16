3b87e51a921dd39b051840d7cc06ea4b99c4019f
porcoRosso <jushan1980@gmail.com>
2020-05-16 23:38:12 +0900

    겟무비

---
+++ graphql/db.js
@@ -0,0 +1,39 @@
+let movies = [
+    {
+        id: "0",
+        name: "Star Wars - The new one",
+        score: 53
+    },
+    {
+        id: "1",
+        name: "Avengers - The new one",
+        score: 8
+    },
+    {
+        id: "2",
+        name: "The Godfather",
+        score: 99
+    },
+    {
+        id: "3",
+        name: "Logan",
+        score: 8
+    }
+];
+
+export const getById = id => {
+    const filteredMovies = movies.filter(movie => movie.id === String(id));
+    return filteredMovies[0];
+};
+
+export const getMovies = () => movies;
+
+export const deleteMovie = (id) => {
+    const cleanedMovies = movies.filter(movie => movie.id !== String(id));
+    if (movies.length > cleanedMovies.length) {
+        movie = cleanedMovies;
+        return true;
+    } else {
+        return false;
+    }
+}
